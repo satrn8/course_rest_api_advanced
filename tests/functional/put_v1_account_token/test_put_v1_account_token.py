f6061fa00d88784b40bcf5ae34b172e7dd6cbadf
satrn8 <verypyc@gmail.com>
2024-06-10 23:49:08 +0300

    правки по тесту

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -1,4 +1,5 @@
 import pprint
+import random
 from json import loads
 
 from dm_api_account.apis.account_api import AccountApi
@@ -12,7 +13,7 @@ def test_post_v1_account():
     login_api = LoginApi(host="http://5.63.153.31:5051")
     mailhog_api = MailhogApi(host="http://5.63.153.31:5025")
 
-    login = "alyona73"
+    login = f"alyona{random.randint(200, 600)}"
     password = "qwerty12345"
     email = f'{login}@mail.ru'
 
