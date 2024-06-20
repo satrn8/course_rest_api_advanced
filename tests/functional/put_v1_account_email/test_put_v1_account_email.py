f3ffa3a7fe8b6187ca138555a135c79b5b77c612
satrn8 <verypyc@gmail.com>
2024-06-20 21:13:45 +0300

    исправления

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -3,7 +3,7 @@ def test_put_v1_account_email(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
-    new_email = "thisisnewmail@mail.ru"
+    new_email = "thisisanewmail1@mail.ru"
     account_helper.register_new_user(login=login, password=password, email=email)
     account_helper.change_email(login=login, password=password, email=new_email)
     account_helper.user_login(login=login, password=password)
