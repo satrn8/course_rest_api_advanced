489a775412bc5e4cac1f7fa65636abe3ba5126a8
satrn8 <verypyc@gmail.com>
2024-06-20 21:16:40 +0300

    исправления

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -1,9 +1,11 @@
+import random
+
 
 def test_put_v1_account_email(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
-    new_email = "thisisanewmail1@mail.ru"
+    new_email = f"thisisanewmail{random.randint(1, 1000)}@mail.ru"
     account_helper.register_new_user(login=login, password=password, email=email)
     account_helper.change_email(login=login, password=password, email=new_email)
     account_helper.user_login(login=login, password=password)
