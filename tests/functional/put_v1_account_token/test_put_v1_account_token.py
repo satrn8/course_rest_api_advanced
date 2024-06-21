610b7ce347b5a2dc2867e87d29484dd5d059c06b
satrn8 <verypyc@gmail.com>
2024-06-21 14:35:52 +0300

    исправления

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -1,4 +1,4 @@
-def test_put_v1_account_token(account_helper, prepare_user, auth_account_helper):
+def test_put_v1_account_token(account_helper, prepare_user):
     login = prepare_user.login
     password = prepare_user.password
     email = prepare_user.email
