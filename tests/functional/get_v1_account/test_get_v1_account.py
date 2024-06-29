65a33a9c9ab3e1545b72eb8e8fd6b1f6c712c555
satrn8 <verypyc@gmail.com>
2024-06-29 23:49:38 +0300

    soft assertions

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,29 +1,16 @@
 import datetime
 
-from hamcrest import assert_that, has_property, has_properties, starts_with, all_of, instance_of, equal_to, has_key
-
+from assertpy import assert_that, soft_assertions
 from checkers.http_checkers import check_status_code_http
+from dm_api_account.models.user_details_envelope import UserRole
 
 
 def test_get_v1_account_auth(auth_account_helper):
-    with check_status_code_http(200):
-        response = auth_account_helper.dm_account_api.account_api.get_v1_account()
-        assert_that(
-            response, all_of(
-                has_property("resource", has_property("login", starts_with("alyona"))),
-                has_property("resource", has_property("registration", instance_of(datetime.datetime))),
-                has_property("resource", has_property("online", instance_of(datetime.datetime))),
-                has_property(
-                    "resource", has_properties({
-                        "roles": [
-                            "Guest",
-                            "Player"
-                        ]
-                    })
-                )
-            ))
-
-        print(response)
+    response = auth_account_helper.dm_account_api.account_api.get_v1_account()
+    with soft_assertions():
+        assert_that(response.resource.login).starts_with('alyona')
+        assert_that(response.resource.online).is_instance_of(datetime.datetime)
+        assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
 
 
 def test_get_v1_account_no_auth(account_helper):
